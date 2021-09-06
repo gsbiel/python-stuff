1c928c80ba3c8de1440c9a902459b0bcf85657d8
Gaspar <gabriel.sgaspar@yahoo.com.br>
2021-09-06 16:43:41 -0300

    adjust code

--- get_fundamental_matrix_from_camera_matrices.py
+++ get_fundamental_matrix_from_camera_matrices.py
@@ -16,13 +16,13 @@ def get_system_calibration_data():
             distortions[cameras[i]] = np.array(data["distortion"]["doubles"])
     return
 
-def undistort(image, mtx, dist):
+def undistort(img, mtx, dist):
     # img = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
     # image = os.path.splitext(image)[0]
     h, w = img.shape[:2]
     newcameramtx, _ = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
     dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
-    return dst
+    return dst, newcameramtx
 
 frames = {}
 camera_combinations = [("0","1"),("0","2"),("0","3"),("1","2"),("1","3"),("2","3")]
@@ -87,14 +87,14 @@ for camera_combination in camera_combinations:
     frame_left = frames[camLeft]
     frame_right = frames[camRight]
 
-    new_intrinsic_left = undistort(frame_left, intrinsic_matrices["cam{}".format(camLeft)], distortions["cam{}".format(camLeft)])
-    new_intrinsic_right = undistort(frame_right, intrinsic_matrices["cam{}".format(camRight)], distortions["cam{}".format(camRight)])
+    frame_left_undistorted, intrinsic_left_undistorted = undistort(frame_left, intrinsic_matrices["cam{}".format(camLeft)], distortions["cam{}".format(camLeft)])
+    frame_right_undistorted, intrinsic_right_undistorted = undistort(frame_right, intrinsic_matrices["cam{}".format(camRight)], distortions["cam{}".format(camRight)])
 
     # camera_left_matrix = intrinsic_matrices["cam{}".format(camLeft)].dot(projection_matrix.dot(extrinsic_matrices["cam{}".format(camLeft)]))
     # camera_right_matrix = intrinsic_matrices["cam{}".format(camRight)].dot(projection_matrix.dot(extrinsic_matrices["cam{}".format(camRight)]))
 
-    camera_left_matrix = new_intrinsic_left.dot(projection_matrix.dot(extrinsic_matrices["cam{}".format(camLeft)]))
-    camera_right_matrix = new_intrinsic_right.dot(projection_matrix.dot(extrinsic_matrices["cam{}".format(camRight)]))
+    camera_left_matrix = intrinsic_left_undistorted.dot(projection_matrix.dot(extrinsic_matrices["cam{}".format(camLeft)]))
+    camera_right_matrix = intrinsic_right_undistorted.dot(projection_matrix.dot(extrinsic_matrices["cam{}".format(camRight)]))
 
     F = cv2.sfm.fundamentalFromProjections(camera_left_matrix,camera_right_matrix)
     fundamental_matrices.append(F)
@@ -103,7 +103,7 @@ output_dict = {}
 for f_matrix, combination in zip(fundamental_matrices, camera_combinations):
     output_dict["{o}-{d}".format(o=combination[0],d=combination[1])] = f_matrix.tolist()
 
-with open("fundamental_matrices.json", 'w') as outfile:
+with open("fundamental_matrices_undistorted.json", 'w') as outfile:
     json.dump(output_dict,outfile)
 
 
